efd81523ea9f9ef9f485b709b83c79d3423cad7d
IlyaTsaplin <ilyacaplin2015@gmail.com>
2020-10-09 11:53:45 +0300

    added smart pointers

--- Tsaplin/Lab1/Source/lab1.cpp
+++ Tsaplin/Lab1/Source/lab1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 
 class Citizen{
 	public:
@@ -9,18 +10,18 @@ class Citizen{
 		this->kids_list = kids_list;
 	}
 
-	bool are_relatives(Citizen& B, Citizen* array,int length, int deep){
+	bool are_relatives(Citizen& B, std::shared_ptr<Citizen[]> array,int length, int deep){
 		if(name == B.name){
 			return false;
 		}
 		if((B.kids_list.find(name) != std::string::npos) || (kids_list.find(B.name) != std::string::npos)){
 			return true;
 		}
-		if(deep==length){
+		if(deep == length){
 			return false;
 		}
-		for(int i = 0;i<length;i++){
-			if((this->are_relatives(array[i],array,length,deep+1)) && (array[i].are_relatives(B,array,length,deep+1))){
+		for(int i = 0; i < length; i++){
+			if((this->are_relatives(array[i], array, length, deep+1)) && (array[i].are_relatives(B, array, length, deep+1))){
 						return true;
 					}
 		}
@@ -29,20 +30,19 @@ class Citizen{
 };
 
 int main(){
-	int  number;
+	int number;
 	std::cin >> number;
-	Citizen* array = new Citizen[number];
-	for(int i=0;i<number;i++){
+	auto array = std::shared_ptr<Citizen[]>(new Citizen[number]);
+	for( int i = 0; i < number; i++ ) {
 		std::cin >> array[i].name;
 		getline(std::cin,array[i].kids_list,'\n');
 	}
-	for(int i=0;i<number;i++){
-		for(int j=i;j<number;j++){
-			if(array[i].are_relatives(array[j],array,number,0)){
-				std::cout<<array[i].name<<" - "<<array[j].name<<"\n";
+	for( int i = 0; i < number; i++ ) {
+		for( int j = i; j < number; j++ ) {
+			if(array[i].are_relatives( array[j], array, number, 0)){
+				std::cout << array[i].name << " - " << array[j].name << "\n";
 			}
 		}
 	}
-	delete[] array;
 	return 0;
 }
