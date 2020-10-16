2d4e391b0a8505f9aeea8a9f3dcc114e1d8d1025
Kraev Denis <Deimo.s@yandex.ru>
2020-10-16 23:48:18 +0300

    [Ready]lab2 - Kraev

---
+++ Kraev/lab2/Source/main.cpp
@@ -0,0 +1,110 @@
+#include "Node.h"
+#include "List.h"
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <fstream>
+#include <unistd.h>
+
+struct globalArgs_t {
+    int noIndex;              
+    char *langCode;            
+    const char *outFileName;    
+    FILE *outFile;
+    int verbosity;              
+    int numInputFiles;          
+    char **inputFiles;          
+} globalArgs;
+
+
+int main(int argc, char** argv){
+	List list("text.txt");
+
+	int opt =0;
+	globalArgs.noIndex = 0;     
+    globalArgs.langCode = NULL;
+    globalArgs.outFileName = NULL;
+    globalArgs.outFile = NULL;
+    globalArgs.verbosity = 0;
+    globalArgs.inputFiles = NULL;
+    globalArgs.numInputFiles = 0;
+	
+	std::vector<int> number, number2;
+    std::string num;
+
+	char* optString = "abcde";
+	opt = getopt( argc, argv, optString );
+    
+	while( opt != -1 ) {
+        switch( opt ) {
+            case 'a':		// добавление элемента
+				list.addElem(argv[2]);
+				break;
+                 
+            case 'b':		// удаление элемента по номеру
+				while(*argv[2] != '\0' ){
+					if(*argv[2] != '.'){
+						num.push_back(*argv[2]);
+					}else{
+						number.push_back(stoi(num));
+						num.clear();
+					}
+					argv[2]++;
+				}
+				number.push_back(stoi(num));
+				list.deleteElem(number);
+				break;
+                 
+            case 'c':		// удаление элемента по названию
+                list.deleteElem(argv[2]);
+				break;
+                 
+            case 'd':		// изменение названия
+                while(*argv[2] != '\0' ){
+                    if(*argv[2] != '.'){
+                        num.push_back(*argv[2]);
+                    }else{
+                        number.push_back(stoi(num));
+                        num.clear();
+                    }
+                    argv[2]++;
+                }
+				number.push_back(stoi(num));
+				list.changeName(number, argv[3]);
+				break;
+                 
+            case 'e':  		// изменение номера
+				while(*argv[2] != '\0' ){
+                    if(*argv[2] != '.'){
+                        num.push_back(*argv[2]);
+                    }else{
+                        number.push_back(stoi(num));
+                        num.clear();
+                    }
+                    argv[2]++;
+                }
+				number.push_back(stoi(num));
+				num.clear();
+				while(*argv[3] != '\0' ){
+                    if(*argv[3] != '.'){
+                        num.push_back(*argv[3]);
+                    }else{
+                        number2.push_back(stoi(num));
+                        num.clear();
+                    }
+                    argv[3]++;
+                }
+                number2.push_back(stoi(num));
+				
+				list.changeNumber(number, number2);
+                break;
+                 
+            default:
+                break;
+        }
+         
+        opt = getopt( argc, argv, optString );
+    }
+	list.print();
+	return 0;
+}
