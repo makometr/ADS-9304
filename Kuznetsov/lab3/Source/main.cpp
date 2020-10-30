eaf3f81b11e4c00dddea85c33936acc60ed97df3
KuznetsovRoman <kuznetsov.roman.orenburg@gmail.com>
2020-10-30 05:48:19 +0300

    Kuznetsov LB3: copy constructor+operator implemented

--- Kuznetsov/lab3/Source/main.cpp
+++ Kuznetsov/lab3/Source/main.cpp
@@ -5,18 +5,10 @@
 #include <queue>
 #include <algorithm>
 
-struct Node;
-using NodePtr = std::shared_ptr<Node>;
 #define invStringArg() {\
 	std::cout << "ERROR: given string tree is invalid";\
 	exit(EXIT_SUCCESS);\
 }
-
-struct Node {
-	int data = 0;
-	NodePtr left, right;
-	Node(int data) :data(data) {}
-};
 int getInt(std::string::iterator& it, const std::string::iterator end) {
 	int val = 0;
 	bool isNegative = false;
@@ -24,50 +16,83 @@ int getInt(std::string::iterator& it, const std::string::iterator end) {
 		isNegative = true;
 		it++;
 	}
-	if(!isdigit(*it) && *it =='(')invStringArg();
-	while (it!=end && isdigit(*it))
+	if (!isdigit(*it) && *it == '(')invStringArg();
+	while (it != end && isdigit(*it))
 		val = val * 10 + *(it++) - '0';
 	if (it == end)invStringArg();
 	return isNegative ? -val : val;
 }
-bool BinSearchCheck(const NodePtr& node) {
-	int min = INT_MIN;
-	bool isBinSearch = true;
-	auto binCheck = [&](const NodePtr & node, auto && binCheck) {
-		if (!node || !isBinSearch)
-			return;
-		binCheck(node->left, binCheck);
-		isBinSearch &= node->data > min;
-		min = node->data;
-		binCheck(node->right, binCheck);
-	};
-	binCheck(node, binCheck);
-	return isBinSearch;
-}
-bool PyramideCheck(const NodePtr &node) {
-	return ((node->left) ? (node->data >= node->left->data) && PyramideCheck(node->left) :true) \
-		&& ((node->right) ? (node->data >= node->right->data) && PyramideCheck(node->right) :true);
-}
-
-int main() {
-	NodePtr tree;
-	{std::string input;
-	std::getline(std::cin, input);
-	std::string::iterator it = input.begin(), end = input.end();
-	auto makeBinTree = [&](NodePtr& node, auto makeBinTree) {
-		if (*it == ')') return;
-		if (*it == '!') {
+template <typename T>
+struct Node {
+	int data = 0;
+	std::shared_ptr<Node> left, right;
+	Node(T data) :data(data) {}
+};
+template <typename T>
+using NodePtr = std::shared_ptr<Node<T>>;
+template <typename T>
+class BinTree {
+public:
+	BinTree() :head(nullptr) {}
+	BinTree(const std::string::iterator begin, const std::string::iterator end) {
+		auto it = begin;
+		auto makeBinTree = [&it, &end](NodePtr<int> & node, auto makeBinTree) -> void {
+			if (*it == ')') return;
+			if (*it == '!') {
+				it++;
+				return;
+			}
+			if (*(it++) != '(' || it == end)invStringArg();
+			node = std::make_shared<Node<int>>(getInt(it, end));
+			makeBinTree(node->left, makeBinTree);
+			makeBinTree(node->right, makeBinTree);
 			it++;
-			return;
+		};
+		makeBinTree(head, makeBinTree);
+	}
+	BinTree& operator=(const BinTree<T> & val) {
+		_copy(head, val.head);
+		return *this;
+	}
+	BinTree(const BinTree<T>& val) {
+		_copy(head, val.head);
+	}
+	bool BinSearchCheck() {
+		int min = INT_MIN;
+		bool isBinSearch = true;
+		auto binCheck = [&isBinSearch, &min](const NodePtr<T> & node, auto && binCheck) {
+			if (!node || !isBinSearch)
+				return;
+			binCheck(node->left, binCheck);
+			isBinSearch &= node->data > min;
+			min = node->data;
+			binCheck(node->right, binCheck);
+		};
+		binCheck(head, binCheck);
+		return isBinSearch;
+	}
+	bool PyramideCheck() {
+		auto _pyramideCheck = [](const NodePtr<T> & node, auto && _pyramideCheck) -> bool {
+			return ((node->left) ? (node->data >= node->left->data) && _pyramideCheck(node->left, _pyramideCheck) : true) \
+				&& ((node->right) ? (node->data >= node->right->data) && _pyramideCheck(node->right, _pyramideCheck) : true);
+		};
+		return _pyramideCheck(head, _pyramideCheck);
+	}
+private:
+	NodePtr<T> head;
+	void _copy(NodePtr<T> & dest, const NodePtr<T> & source){
+		if (source) {
+			dest = std::make_shared<Node<T>>(source->data);
+			_copy(dest->left, source->left);
+			_copy(dest->right, source->right);
 		}
-		if (*(it++) != '(' || it == end)invStringArg();
-		node = std::make_shared<Node>(getInt(it, end));
-		makeBinTree(node->left, makeBinTree);
-		makeBinTree(node->right, makeBinTree);
-		it++;
 	};
-	makeBinTree(tree, makeBinTree);}
-	std::cout << "Binsearch:\t" << (BinSearchCheck(tree) ? "Yes" : "No") \
-		<< "\nPyramid:\t" << (PyramideCheck(tree) ? "Yes" : "No") << '\n';
+};
+int main() {
+	std::string input;
+	std::getline(std::cin, input);
+	BinTree<int> tree(input.begin(), input.end());
+	std::cout << "Binsearch:\t" << (tree.BinSearchCheck() ? "Yes" : "No") \
+		<< "\nPyramid:\t" << (tree.PyramideCheck() ? "Yes" : "No") << '\n';
 	return 0;
 }
