0f8e4c7c920b89a1ba704ac677f7679cbac9a234
strax1e <strax1e@yandex.ru>
2020-11-18 15:20:55 +0300

    lab 4 pdf update and sort update

--- Afanasyev/lab4/Sources/libs/IterQSort.h
+++ Afanasyev/lab4/Sources/libs/IterQSort.h
@@ -1,11 +1,12 @@
 #ifndef __ITERQSORT__H__
 #define __ITERQSORT__H__
 
-#include <stack>
 #include <iostream>
+#include <stack>
+#include <utility>
 
 template <typename RandomIt>
-void swapForSort(RandomIt left, RandomIt right)
+void swapObjs(RandomIt left, RandomIt right)
 {
     auto tmpSwap = *left;
     *left = *right;
@@ -13,56 +14,77 @@ void swapForSort(RandomIt left, RandomIt right)
 }
 
 template <typename RandomIt>
+RandomIt partition(RandomIt left, RandomIt right)
+{
+    RandomIt pivotPosSelector = left;
+    RandomIt pivot = right;
+
+    for (auto it = left; it != pivot; ++it)
+    {
+        if (*it < *pivot)
+        {
+            if (it != pivotPosSelector)
+                swapObjs<RandomIt>(it, pivotPosSelector);
+            ++pivotPosSelector;
+        }
+    }
+    // ставим pivot на свое место
+    swapObjs<RandomIt>(pivot, pivotPosSelector);
+    return pivotPosSelector;
+}
+
+template <typename RandomIt>
 void iterQSort(RandomIt start, RandomIt end)
 {
     if (start < end)
     {
+        std::stack<std::pair<RandomIt, RandomIt>> stck;
+        stck.push(std::make_pair<RandomIt, RandomIt>(std::move(start), std::move(end)));
 
-        std::stack<RandomIt> stck;
-        stck.push(end);
-        stck.push(start);
-
+        // счетчик шагов
         int counter = 0;
 
-        std::cout << "Step " + std::to_string(counter) + ":\n";
+        // вывод на экран шага
+        std::cout << "Step 0:\n";
         for (auto it = start; it != end; ++it)
             std::cout << *it << ' ';
         std::cout << '\n';
+
         while (!stck.empty())
         {
             ++counter;
-            RandomIt newStart = stck.top();
+            std::pair<RandomIt, RandomIt> borders = stck.top();
             stck.pop();
 
-            RandomIt newEnd = stck.top();
-            stck.pop();
+            RandomIt pivot = partition(borders.first, borders.second - 1);
 
-            RandomIt iterToCompareWithPivot = newStart;
-            RandomIt pivot = newEnd - 1;
-
-            for (auto it = newStart; it != newEnd; ++it)
-            {
-                if (*it < *pivot)
-                {
-                    swapForSort<RandomIt>(it, iterToCompareWithPivot);
-                    ++iterToCompareWithPivot;
-                }
-            }
-            swapForSort<RandomIt>(pivot, iterToCompareWithPivot);
+            // вывод на экран шага
             std::cout << "Step " + std::to_string(counter) + ":\n";
             for (auto it = start; it != end; ++it)
                 std::cout << *it << ' ';
             std::cout << '\n';
 
-            if (iterToCompareWithPivot - 1 > newStart)
+            // если оба отрезка длиннее 1-го элемента
+            if ((pivot - 1 > borders.first) && (pivot + 1 < borders.second - 1))
             {
-                stck.push(iterToCompareWithPivot);
-                stck.push(newStart);
+                // больший отрезок первым кладем на стек. Так мы в среднем уменьшим наибольшую высоту стека на один.
+                if (((pivot - 1) - borders.first) > (borders.second - 1) - (pivot + 1))
+                {
+                    stck.push(std::make_pair<RandomIt, RandomIt>(std::move(std::get<0>(borders)), std::move(pivot)));
+                    stck.push(std::make_pair<RandomIt, RandomIt>(pivot + 1, std::move(std::get<1>(borders))));
+                }
+                else
+                {
+                    stck.push(std::make_pair<RandomIt, RandomIt>(pivot + 1, std::move(std::get<1>(borders))));
+                    stck.push(std::make_pair<RandomIt, RandomIt>(std::move(std::get<0>(borders)), std::move(pivot)));
+                }
             }
-            if (iterToCompareWithPivot + 1 < newEnd)
+            else
             {
-                stck.push(newEnd);
-                stck.push(iterToCompareWithPivot + 1);
+                if (pivot - 1 > borders.first)
+                    stck.push(std::make_pair<RandomIt, RandomIt>(std::move(std::get<0>(borders)), std::move(pivot)));
+                if (pivot + 1 < borders.second - 1)
+                    stck.push(std::make_pair<RandomIt, RandomIt>(pivot + 1, std::move(std::get<1>(borders))));
             }
         }
     }
