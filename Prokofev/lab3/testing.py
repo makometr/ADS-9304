07b79ec4a8fbd3e0e1dce8d64aac5b4875f1bcac
maiklProko@gmail.com <maiklProko@gmail.com>
2020-12-14 12:57:52 +0300

    [FIXED] lab3 - Prokofev

---
+++ Prokofev/lab3/testing.py
@@ -0,0 +1,45 @@
+import unittest
+import subprocess
+import os
+import sys
+
+
+class TestLab(unittest.TestCase):
+
+	def SuccessTest(self):
+		with open('Tests/SuccessTests.txt', 'r') as file:
+			print('\nSuccess tests:\n')
+			while True:
+				ln = file.readline()
+				ln2 = file.readline()
+				fl = open('input.txt', 'w')
+				fl.write(ln)
+				fl.write(ln2)
+				fl.close()
+				os.system(name)
+				print('\n--------------------------------\n')
+				d = file.readline()
+				if not d:
+					break
+
+	def ErrorTest(self):
+		with open('Tests/ErrorTests.txt', 'r') as file:
+			print('\nError tests:\n')
+			while True:
+				ln = file.readline()
+				ln2 = file.readline()
+				fl = open('input.txt', 'w')
+				fl.write(ln)
+				fl.write(ln2)
+				fl.close()
+				os.system(name)
+				print('\n--------------------------------\n')
+				d = file.readline()
+				if not d:
+					break
+
+
+name = sys.argv[1]
+tt = TestLab()
+tt.SuccessTest()
+tt.ErrorTest()
